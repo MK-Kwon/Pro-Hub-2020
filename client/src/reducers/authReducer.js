ecc1e65d20ff10a01527e28e7e84decea5b42b33
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-12-08 18:05:32 +1030

    Oauth template

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,6 @@
 import { FETCH_USER } from '../actions/types'
 
-export default function(state = null, action) {
+const User = function(state = null, action) {
     // console.log(action)
     switch (action.type) {
         case FETCH_USER:
@@ -9,3 +9,5 @@ export default function(state = null, action) {
         return state;
     }
 } 
+
+export default User;
