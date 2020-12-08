a178d9ffe84445aa6024436b29382bfb1c3cc26f
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-12-08 19:25:37 +1030

    added background img

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -2,13 +2,17 @@
 
 import React from 'react';
 
-
 const Hero = () => {
     return (
-        <div className="uk-cover-container">
-            <img src={require("../images/pawel-czerwinski-QRghuf5yTA4-unsplash_optimized.jpg")} alt="watercolor background" />
-        </div>
+            <div id="carousel" className="uk-position-relative uk-visible-toggle uk-light" tabIndex="-1" uk-slideshow="autoplay: true; animation: fade">
+            <ul className="uk-slideshow-items">
+                
+                <img className="carousel_img" src={require("../images/clement-falize-9uNFBOXBEBc-unsplash_optimized.jpg")} alt="typing on laptop" uk-cover="true" />
+            </ul>
+            </div>
     )
+    
 }
 
-export default Hero; 
+
+export default Hero;
