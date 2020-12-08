7814143b930966899b46d48768bf77d662bb97f0
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-12-08 18:43:09 +1030

    added hero component

---
+++ client/src/components/Hero.js
@@ -0,0 +1,14 @@
+// Welcome to the Dream Team's Co-Lab Project! This is our landing page.
+
+import React from 'react';
+
+
+const Hero = () => {
+    return (
+        <div className="uk-cover-container">
+            <img src="../images/clement-falize-9uNFBOXBEBc-unsplash_optimized.jpg" alt="watercolor background" />
+        </div>
+    )
+}
+
+export default Hero; 
