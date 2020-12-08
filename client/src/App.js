6697288fde13a3e7eb07c4f7c1c5050c314deb9b
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-12-08 11:19:42 +1030

    added header and routing for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <SignUp />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path={["/", "/signup"]}>
+            <SignUp />
+          </Route>
+          <Route exact path="/signin">
+            <SignIn />
+          </Route>
+          {/* <Route>
+            <NoMatch />
+          </Route> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
+export default App; 
