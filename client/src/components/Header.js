e591cea37044e00208a93ff7c86b8e60a81bbc5e
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-12-12 20:20:47 +1030

    fixed layout on CreateTeam

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -27,7 +27,7 @@ class Header extends Component {
     render() {
         return (
             <nav>
-                <div className="nav-wrapper uk-sticky">
+                <div className="nav-wrapper">
                     <Link style={{ margin: "0 20px", fontSize: "1.2rem" }}
                         to={'/dashboard'}
                         className="left-brand-logo">
