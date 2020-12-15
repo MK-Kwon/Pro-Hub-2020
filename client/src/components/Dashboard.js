9d54d7edf15d256d59324e49aab6db4e5e246d5b
JLee3849023 <JLee3849023@gmail.com>
2020-12-15 16:48:44 +1030

    made it so you can link your github acc which will populate more info in your profile

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,10 +7,26 @@ import * as actions from '../actions'
 import { motion } from "framer-motion"
 import API from "../utils/API";
 
+
 const Dashboard = props => {
+  const user = props.auth || {};
   // var link_github_b_e = document.getElementById("link_github_b")
   var link_github_b_e = document.querySelector("#link_github_b")
   const [username, setUsername] = useState("");
+  const [usernameElement, setUsernameElement] = useState(<input
+    id="username_i"
+    tyoe="text"
+    placeholder="Enter Github Username"
+    onBlur={(e) => e.target.placeholder = "Enter Github Username"}
+    onFocus={(e) => e.target.placeholder = ""}
+    onChange={(e) => setUsername(e.target.value)}>
+  </input>);
+  const [bio, setBio] = useState("");
+  const [githubData, setGithubData] = useState("");
+  console.log(user.photo)
+  const picture = user.photo
+  const [userPhoto, setUserPhoto] = useState(picture);
+  const [linkGithubButton, setlinkGithubButton] = useState("Link your Github Account");
 
   const pageVariants = {
     initial: {
@@ -30,8 +46,6 @@ const Dashboard = props => {
     }
   };
 
-  const user = props.auth || {};
-
   const get_github = (username) => {
     // console.log("hello")
     // if (link_github_b_e !== null) {
@@ -42,12 +56,20 @@ const Dashboard = props => {
       .then(res => {
         console.log(res.data)
         const github = res.data
+        setBio(github.bio)
+        setUsernameElement([
+          <p key="1" id="username_lable_p">Github Username:</p>,
+          <p key="2" id="username_p" style={{ width: "70%", margin: "20px 0px" }}>{github.login} </p>
+        ])
+        setGithubData(<p key="1">Followers: {github.followers} Following: {github.following} Repos: {github.public_repos}</p>)
+        setUserPhoto(github.avatar_url)
       })
       .catch(err => console.log(err));
   };
   const handleGithubClick = event => {
     event.preventDefault();
     get_github(username);
+    setlinkGithubButton("Unlink Github Account")
   };
 
 
@@ -61,7 +83,7 @@ const Dashboard = props => {
           <div className="uk-card-header">
             <div className="uk-grid-medium uk-flex-middle uk-grid">
               <div className="uk-width-auto">
-                <img className="uk-border-circle" width="40" height="40" alt="profile pic" src={user.photo}></img>
+                <img className="uk-border-circle" width="40" height="40" alt="profile pic" src={userPhoto || user.photo}></img>
               </div>
               <div className="uk-width-expand">
                 <h2 id="profileName" className="uk-card-title uk-margin-remove-bottom">{user.first_name} {user.last_name}</h2>
@@ -72,34 +94,30 @@ const Dashboard = props => {
           <div className="uk-card-body">
             {/* <p>Github: </p> */}
             <div id="location_div">
-              <input
-                id="city_i"
-                tyoe="text"
-                placeholder="Enter Github Username"
-                onBlur={(e) => e.target.placeholder = "Enter Github Username"}
-                onChange={(e) => setUsername(e.target.value)}>
-                onFocus={(e) => e.target.placeholder = ""}
-                onChange={(e) => setUsername(e.target.value)}
-              </input>
+              <div id="username_div">
+                {usernameElement}
+              </div>
               <button
                 id="link_github_b"
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}
                 onClick={handleGithubClick}
-                className="uk-button uk-button-secondary uk-button-large uk-flex-left buttons">Link your Github Account</button>
+                className="uk-button uk-button-secondary uk-button-large uk-flex-left buttons">{linkGithubButton}</button>
             </div>
             <hr></hr>
             <p>Email: {user.email}</p>
             <hr></hr>
-            <p>Bio: </p>
+            {githubData}
+            <p>Bio:</p>
             <textarea
               className="description_t"
               type="text"
+              value={bio}
               placeholder="Describe yourself here " o
               nBlur={(e) => e.target.placeholder = "Describe yourself here "}
               onFocus={(e) => e.target.placeholder = ""}>
             </textarea>
-            <p>Skills: </p>
+            <p>Skills:</p>
             <textarea
               className="description_t"
               type="text"
