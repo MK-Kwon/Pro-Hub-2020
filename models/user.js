367a9793415b7e52a8ca2b238f6f798436639445
MK-Kwon <tolitoli_78@hotmail.com>
2020-12-16 20:49:29 +1030

    added more data into schema

--- models/user.js
+++ models/user.js
@@ -2,21 +2,26 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    googleId: String,
-    credits: {
-      type: Number,
-      default: 0
-    },
-    username: String,
-    name: String,
-    email: String,
-    image: String,
-    location: String,
-    skills: Array,
-    description: String,
-    Team_id: String
+  googleId: String,
+  credits: {
+    type: Number,
+    default: 0
+  },
+  github_username: String,
+  first_name: String,
+  last_name: String,
+  email: String,
+  photo: String,
+  github_photo: String,
+  location: String,
+  skills: String,
+  bio: String,
+  followers: Number,
+  following: Number,
+  repos: Number,
+  github_url: String  
 });
 
 const User = mongoose.model('users', userSchema);
 
-module.exports = User; 
+module.exports = User;
