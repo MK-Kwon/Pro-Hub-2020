c36118365d89b370078d29edb279fa882e924bd6
JLee3849023 <JLee3849023@gmail.com>
2020-12-17 00:45:15 +1030

    added 'update user

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,22 @@
 import axios from 'axios'
-import { FETCH_USER } from './types';
+import { FETCH_USER, UPDATE_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
-    const res = await axios.get('/api/current_user')
-    dispatch({ type: FETCH_USER, payload: res.data })
+  const res = await axios.get('/api/current_user')
+  dispatch({ type: FETCH_USER, payload: res.data })
 }
 
 export const handleToken = (token) => async dispatch => {
-    const res = await axios.post('api/stripe', token)
-    dispatch({ type: FETCH_USER, payload: res.data })
+  const res = await axios.post('api/stripe', token)
+  dispatch({ type: FETCH_USER, payload: res.data })
+}
+
+// go here
+export const updateUser = (id, data) => async dispatch => {
+  // console.log("actions index.js")
+  // console.log(id)
+  // console.log(data)
+  const res = await axios.put("/api/user/" + id, data);
+  dispatch({ type: UPDATE_USER, payload: res.data })
+
 }
