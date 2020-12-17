df44ed4336be176f4f6eef1c13a30cb23147a17b
JLee3849023 <JLee3849023@gmail.com>
2020-12-17 16:09:01 +1030

    added project profile and search project

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import Hero from "./Hero";
+import Hero from "../Hero";
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
-const Landing = () => {
-    return (
-        <div style={{ textAlign: 'center' }}>
-            <Hero />
-        </div>
-    )
+const Landing = (props) => {
+  console.log(props)
+  return (
+    <Hero />
+  )
 }
 
-export default Landing; 
+function get_user_data({ auth }) {
+  return { auth }
+}
+
+export default connect(get_user_data, actions)(Landing);
