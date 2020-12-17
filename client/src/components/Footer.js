f4a9770828f29a9f99a3d0eb0f77de13c09f3725
JLee3849023 <JLee3849023@gmail.com>
2020-12-17 22:52:30 +1030

    updated Hero page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div id="footer">
-                <a href="https://github.com/bwmccray/Co-Lab"><p>©Pro-Hub-2020 Designed by Min Kyung Kwon and James Lee.</p></a>
+                <a href="https://github.com/MK-Kwon/Pro-Hub-2020"><p>©Pro-Hub-2020 Designed by Min Kyung Kwon and James Lee.</p></a>
             </div>
 
         )
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,45 +1,74 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import Payments from './Payments'
-import { motion } from "framer-motion"
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
 
-class Header extends Component {
 
-  renderContent() {
-    switch (this.props.auth) {
+const Header = props => {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  //========================================================
+  // Grabbing information for the nav bar.
+  //========================================================
+  const renderContent = () => {
+
+    // Grabbing user's name
+    const user = props.auth || {};
+
+    switch (props.auth) {
       case null:
         return;
       case false:
         return (
-
           <li><a href="/auth/google">Login With Google</a></li>)
       default:
         return [
-          <li key="1"><Payments /></li>,
-          <li key="3" style={{ margin: '0 10px' }}>
-            Credits: {this.props.auth.credits}
-          </li>,
-          <li key="2"><a href="/api/logout">Logout</a></li>
+          <button key="menuButton" className="menuButton" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            Hello, {user.first_name}
+          </button>,
+          <Menu
+            id="simple-menu"
+            key="simpleMenu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}>
+            <Link key="1" to="/profile"><MenuItem onClick={handleClose} >Profile</MenuItem></Link>
+            <Link key="2" to="/createproject"><MenuItem onClick={handleClose}>Create Project</MenuItem></Link>
+            <Link key="5" to="/projectProfile"><MenuItem onClick={handleClose}>Your Projects</MenuItem></Link>
+            <Link key="3" to="/searchusers"><MenuItem onClick={handleClose}>Search Users</MenuItem></Link>
+            <MenuItem key="4" onClick={handleClose}><a href="/api/logout">Logout</a></MenuItem>
+          </Menu>
+        ]
     }
   }
-  render() {
-    return (
-      <nav>
-        <div className="nav-wrapper">
-          <Link style={{ margin: "0 20px", fontSize: "1.2rem" }}
-            to={'/dashboard'}
-            className="left-brand-logo">
-            Home
-          </Link>
-          <ul id="nav-mobile" className="right">
-            {this.renderContent()}
-          </ul>
-        </div>
-      </nav >
-    )
-  }
+
+
+  return (
+
+    <nav>
+      <div className="nav-wrapper">
+        <Link
+          to={'/'}
+          className="left-brand-logo menuButton ">Pro-Hub-2020</Link>
+        <ul id="nav-mobile" className="right">
+          {renderContent()}
+        </ul>
+      </div>
+    </nav >
+  )
 };
+
 function mapStateToProps({ auth }) {
   return { auth };
 }
